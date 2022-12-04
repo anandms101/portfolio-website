7f4fabe83eebf785d3211a69d662c6f1c7729da2
ANAND MOHAN SINGH <anandmohan.singh101@gmail.com>
2022-12-04 16:02:36 +0530

    about me page tech
    added the technologies icons in about me

---
+++ portfolio_website/src/components/constants.js
@@ -0,0 +1,86 @@
+import { Icon } from "@iconify/react";
+
+export const technology = [
+  {
+    name: "Java",
+    componentName: "logos:java",
+  },
+  {
+    name: "JavaScript",
+    componentName: "logos:javascript",
+  },
+  {
+    name: "C++",
+    componentName: "gg:c-plus-plus",
+  },
+  {
+    name: "C#",
+    componentName: "teenyicons:c-sharp-solid",
+  },
+  {
+    name: "C",
+    componentName: "teenyicons:c-solid",
+  },
+
+  {
+    name: "React",
+    componentName: "logos:react",
+  },
+  {
+    name: "MongoDB",
+    componentName: "vscode-icons:file-type-mongo",
+  },
+  {
+    name: "HTML",
+    componentName: "vscode-icons:file-type-html",
+  },
+  {
+    name: "CSS",
+    componentName: "vscode-icons:file-type-css",
+  },
+  {
+    name: "MySQL",
+    componentName: "logos:mysql",
+  },
+  {
+    name: "Python",
+    componentName: "vscode-icons:file-type-python",
+  },
+  {
+    name: "PHP",
+    componentName: "vscode-icons:file-type-php",
+  },
+  {
+    name: "TypeScript",
+    componentName: "vscode-icons:file-type-typescript-official",
+  },
+  {
+    name: "XML",
+    componentName: "bi:filetype-xml",
+  },
+  {
+    name: "Postman",
+    componentName: "logos:postman-icon",
+  },
+  {
+    name: "GitHub",
+    componentName: "mdi:github",
+  },
+  {
+    name: "jQuery",
+    componentName: "logos:jquery",
+    height: "52",
+    width: "80",
+  },
+  {
+    name: "Tail Wind CSS",
+    componentName: "logos:tailwindcss-icon",
+    height: "50",
+    width: "60",
+  },
+  {
+    name: "R",
+    componentName: "mdi:language-r",
+    // height: "40",
+  },
+];
